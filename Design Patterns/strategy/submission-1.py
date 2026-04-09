b802e2f1c35758f3de56c39c23aa4ebaca52e1d5
Michael Xu <75459900+MichaelXu27@users.noreply.github.com>
2026-04-09 17:06:31 -0500

    Add: strategy - submission-1

---
+++ Design Patterns/strategy/submission-1.py
@@ -0,0 +1,54 @@
+class Person:
+    def __init__(self, lastName: str, age: int, married: bool):
+        self.lastName = lastName
+        self.age = age
+        self.married = married
+
+    def getLastName(self) -> str:
+        return self.lastName
+
+    def getAge(self) -> int:
+        return self.age
+
+    def isMarried(self) -> bool:
+        return self.married
+
+class PersonFilter(Protocol):
+    def apply(self, person: Person) -> bool:
+        ...
+
+class AdultFilter(PersonFilter):
+    def apply(self, person: Person) -> bool:
+        if person.getAge() >= 18:
+            return True
+        else:
+            return False
+
+class SeniorFilter(PersonFilter):
+    def apply(self, person: Person) -> bool:
+        if person.getAge() >= 65:
+            return True
+        else:
+            return False
+
+class MarriedFilter(PersonFilter):
+    def apply(self, person: Person) -> bool:
+        if person.isMarried():
+            return True
+        else:
+            return False
+
+class PeopleCounter:
+    def __init__(self):
+        self.filter: PersonFilter = None
+
+    def setFilter(self, filter: PersonFilter) -> None:
+        self.filter = filter
+
+    def count(self, people: List[Person]) -> int:
+        count = 0
+        for person in people:
+            if self.filter.apply(person):
+                count += 1
+        return count
+    
